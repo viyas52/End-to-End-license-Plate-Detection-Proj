b8cd4998c8b58a03ce59f5374e4653ec4fbe82ae
viyas52 <vyas.illusionist@gmail.com>
2024-08-19 23:37:36 +0530

    setup created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'LicensePlate',
+    version= '0.0.0',
+    author= 'viyas52',
+    author_email= 'vyas.illusionist@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
