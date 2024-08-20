37ad6b3f7158e73fc5cbff510c31499bf760f21d
viyas52 <vyas.illusionist@gmail.com>
2024-08-20 13:04:50 +0530

    data validation added

--- LicensePlate/constant/training_pipeline/__init__.py
+++ LicensePlate/constant/training_pipeline/__init__.py
@@ -13,3 +13,15 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/10V1rsYPicG9u6UXMOxGJYK4fI0U0_RG7/view?usp=drive_link"
 
 DATA_FILE_NAME:str = "LicensePlate_Data.zip"
+
+
+
+"""
+Data Validation 
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["test", "train", "data.yaml"]
--- LicensePlate/pipeline/training_pipeline.py
+++ LicensePlate/pipeline/training_pipeline.py
@@ -4,7 +4,7 @@ from LicensePlate.logger import logging
 from LicensePlate.exception import CustomException
 
 from LicensePlate.components.data_ingestion import DataIngestion
-
+from LicensePlate.components.data_validation import DataValidation
 
 from LicensePlate.entity.config_entity import *
 from LicensePlate.entity.artifacts_entity import *
@@ -12,7 +12,7 @@ from LicensePlate.entity.artifacts_entity import *
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-        
+        self.data_validation_config = DataValidationConfig()
     
     def start_data_ingestion(self)-> DataIngestionArtifact:
         try: 
@@ -37,10 +37,33 @@ class TrainPipeline:
             raise CustomException(e, sys)
         
         
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=self.data_validation_config,)
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
+        
+    
+        
 
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            
             
         except Exception as e: 
             raise CustomException(e,sys)
